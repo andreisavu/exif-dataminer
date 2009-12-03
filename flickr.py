1ff4b1d9a37080e95632c1e17504b7fc2797bebf
Andrei Savu <savu.andrei@gmail.com>
2009-12-03 02:25:01 +0200

    initial commit

---
+++ flickr.py
@@ -0,0 +1,51 @@
+"""
+Simple Flickr API built using YQL
+
+You need to install python-yql in order to be able to use this.
+All the calls are public and require no authentication.
+"""
+
+import yql
+
+class Flickr(object):
+    def __init__(self):
+        self.yql = yql.Public()
+
+    def interesting_pictures(self, num=5):
+        """ Fetch most interesting pictures from flickr
+
+        Params: num - result size
+        Yields tuples (id, title)
+        """
+        query = 'select id, title from flickr.photos.interestingness(%s)' % num
+        results = self.yql.execute(query)
+        for row in results.rows:
+            yield (row.get('id'), row.get('title'))
+
+    def get_exif(self, photo_id):
+        """ Fetch exif data for a given photo_id
+
+        Params: flickr photo ID
+        Yields tubles (tag, label, raw_value)
+        """
+        query = 'select * from flickr.photos.exif where photo_id=@id'
+        results = self.yql.execute(query, {'id': photo_id})
+        if results.rows != []:
+            for exif in results.rows['exif']:
+                yield (exif['tag'], exif['label'], exif['raw'])
+
+    def get_photo_urls(self, photo_id):
+        """ Fetch flick photo urls
+
+        Params: flickr photo ID
+        Returns: dict {Size: Url}
+        """
+        query = 'select label, source from flickr.photos.sizes where photo_id=@id'
+        results = self.yql.execute(query, {'id': photo_id})
+        
+        ret = {}
+        for row in results.rows:
+            ret[row['label']] = row['source']
+        return ret
+
+
---
+++ flickr_interesting_dump.py
@@ -0,0 +1,21 @@
+
+import flickr
+
+from itertools import izip, count
+
+f = flickr.Flickr()
+
+for id, title in f.interesting_pictures(3):
+    print 'ID:', id
+    print 'Title:', title
+    print 'Medium URL:', f.get_photo_urls(id)['Medium']
+
+    print "\nExif info:"
+    for index, (tag, label, raw) in izip(count(), f.get_exif(id)):
+        print tag, label, raw
+        if index == 5: 
+            print "... and many more\n\n"
+            break
+    else:
+        print "info not available!\n\n"
+
